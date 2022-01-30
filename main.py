e351c341ca180e1c17479cb6156536d6bbc59d56
UNKNOWN8884 <97676159+UNKNOWN8884@users.noreply.github.com>
2022-01-30 13:06:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -454,15 +454,12 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
                 [
                   
 						InlineKeyboardButton("ՏႮᏢᏢϴᎡͲ ♥️", url="https://t.me/mksupport1")
+                                                InlineKeyboardButton("ᏟϴᎷᎷᎪΝᎠՏ❓", callback_data="help")
 					],
                                         [
-						InlineKeyboardButton("ᏟϴᎷᎷᎪΝᎠՏ❓", callback_data="help")
-                                        ],
-                                        [
-						InlineKeyboardButton("tᎪᏴϴႮͲ 😽", callback_data="about")
-			                ],
-                                        [
-						InlineKeyboardButton("🌬️ᏟᏞϴՏᎬ", callback_data="closeMeh") 
+					        InlineKeyboardButton("tᎪᏴϴႮͲ 😽", callback_data="about")	
+                                                InlineKeyboardButton("🌬️ᏟᏞϴՏᎬ", callback_data="closeMeh") 
+						
 					]
                 ]
             )
